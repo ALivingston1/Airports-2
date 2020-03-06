a401e89f334aa4099a47807a0d4c4d2b6939c226
Alex Livingston <alivingston@techvalleyhigh.org>
2020-03-06 09:20:45 -0500

    Added Airport Class

---
+++ src/Airport.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Airport {
+    private boolean towered;
+    private int numRunways;
+
+    private int capacity;
+    private ArrayList<Aircraft> aircraft = new ArrayList<Aircraft>();
+}
