56d842a1a4f7c772dc458526ab3043e93b618836
Halietheboss <58736321+Halietheboss@users.noreply.github.com>
2020-03-25 16:28:10 -0400

    update logston

--- src/System/Aircraft/CargoTransporter/CargoTransporter.java
+++ src/System/Aircraft/CargoTransporter/CargoTransporter.java
@@ -1,4 +1,48 @@
 package System.Aircraft.CargoTransporter;
 
+import System.Aircraft.CargoTransporter.Package;
+
+import java.util.ArrayList;
+
 public class CargoTransporter {
+
+
+    private String make;
+    private String model;
+    private Double capacity;
+
+    private int[] cargo;
+
+
+    CargoTransporter(String make,double capacity,String model,int[] cargo){
+        this.make=make;
+        this.capacity=capacity;
+        this.model=model;
+        this.cargo=cargo;
+
+    }
+
+
+
+    @Override
+    public String getMake() {
+        return make;
+    }
+
+    @Override
+    public String getModel() {
+        return model;
+    }
+
+    public Double getCapacity(){
+        return capacity;
+    }
+
+    public int[] getCargo() {
+        return package;
+    }
+
 }
+
+
+
--- src/System/Aircraft/CargoTransporter/Package.java
+++ src/System/Aircraft/CargoTransporter/Package.java
@@ -1,4 +1,23 @@
 package System.Aircraft.CargoTransporter;
 
 public class Package {
+
+
+    private double weight;
+    private String destination;
+
+    Package(double weight, String destination){
+        this.destination=destination;
+        this.weight=weight;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+
+
 }
